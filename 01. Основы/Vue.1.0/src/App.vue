4b529963ac1f73445fb3e22deacce6ceaa5b7f86
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-27 00:13:57 +0700

    Массивы

--- 01. Основы/Vue.1.0/src/App.vue
+++ 01. Основы/Vue.1.0/src/App.vue
@@ -2,18 +2,18 @@
 export default {
   data () {
     return {
-      num: 5,
-      num1: 1,
-      num2: 2,
-      num3: 3
+      arr: [1, 2, 3],
+      arr1: ['x', 'y', 'z']
     }
   }
 }
 </script>
 
 <template>
-  <p>{{ num * num }}</p>
-  <div>{{ num1 + num2 + num3 }}</div>
+  <p>{{ arr[0] + arr[1] + arr[2]}}</p>
+  <p>{{ arr1[0] }}</p>
+  <p>{{ arr1[1] }}</p>
+  <p>{{ arr1[2] }}</p>
 </template>
 
 <style scoped></style>
