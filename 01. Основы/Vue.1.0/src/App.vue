c09b62b3f3877dd182c04bf17058c93b213d3bce
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-27 00:16:32 +0700

    Объекты

--- 01. Основы/Vue.1.0/src/App.vue
+++ 01. Основы/Vue.1.0/src/App.vue
@@ -2,18 +2,14 @@
 export default {
   data () {
     return {
-      arr: [1, 2, 3],
-      arr1: ['x', 'y', 'z']
+      obj: {a: 1, b:2, c:3}
     }
   }
 }
 </script>
 
 <template>
-  <p>{{ arr[0] + arr[1] + arr[2]}}</p>
-  <p>{{ arr1[0] }}</p>
-  <p>{{ arr1[1] }}</p>
-  <p>{{ arr1[2] }}</p>
+  <div>{{ obj['a'] + obj['b'] + obj['c'] }}</div>
 </template>
 
 <style scoped></style>
