d7c88195fc47834fd8a4550e486f51b950961677
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-27 16:44:25 +0700

    Атрибут key

--- 01. Основы/Vue.1.0/src/App.vue
+++ 01. Основы/Vue.1.0/src/App.vue
@@ -2,14 +2,27 @@
 export default {
   data() {
     return {
-      items: [1, -2, 3, -4, 5],
+      products: [
+        {
+          id: 1,
+          name: "product1",
+        },
+        {
+          id: 2,
+          name: "product2",
+        },
+        {
+          id: 3,
+          name: "product3",
+        },
+      ],
     };
   },
 };
 </script>
 
 <template>
-  <ul v-for="elem in items">
-    <li v-if="elem > 0">{{ elem }}</li>
+  <ul>
+    <li v-for="elem in products" :key="elem.id">{{ elem.name }}</li>
   </ul>
 </template>
