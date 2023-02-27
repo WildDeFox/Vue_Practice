5911106c5c9bad3396026016a3055219b3953d69
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-27 15:22:10 +0700

    Вычисляемые свойства

--- 01. Основы/Vue.1.0/src/App.vue
+++ 01. Основы/Vue.1.0/src/App.vue
@@ -2,22 +2,25 @@
 export default {
   data() {
     return {
-      text: 'Hello world!',
-      num1: 5,
-      num2: 5
+      cost: 154,
+      amount: 1
     }
   },
   methods: {
-   change: function(newText) {
-    this.text = newText;
+   change: function() {
+    this.amount++
    }
+  },
+  computed: {
+    price: function() {
+      return this.cost * this.amount;
+    }
   }
 }
 </script>
 
 <template>
-  <div>{{ text }}</div>
-  <button @click="change('How are you?')">Click Me!</button>
-  <button @click="change('I am fine!')">Let`Go</button>
+  <div>{{ price }}</div>
+  <button @click="change()">Ещё один!</button>
 </template>
 
