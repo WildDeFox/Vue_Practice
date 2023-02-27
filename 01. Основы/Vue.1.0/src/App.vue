ef18956dfb7c4ad5226883294dcd330cc7c46ef3
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-27 16:36:43 +0700

    Перебор массива объектов

--- 01. Основы/Vue.1.0/src/App.vue
+++ 01. Основы/Vue.1.0/src/App.vue
@@ -2,22 +2,20 @@
 export default {
   data() {
     return {
-      items: [1, 2, 3],
+      hrefs: [
+        { href: "1.html", text: "text1" },
+        { href: "2.html", text: "text2" },
+        { href: "3.html", text: "text3" },
+      ],
     };
-  },
-  methods: {
-    hide: function () {
-      this.visible = !this.visible;
-    },
   },
 };
 </script>
 
 <template>
   <ul>
-    <template v-for="elem in items">
-      <li>{{ elem }}</li>
-      <li class="divider"></li>
+    <template v-for="elem in hrefs">
+      <li><a :href="elem.href">{{ elem.text }}</a></li>
     </template>
   </ul>
 </template>
