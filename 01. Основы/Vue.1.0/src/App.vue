462fb1d5b737d3afcd7ec19a2cbb55d4e901543c
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-27 15:53:25 +0700

    Тогглинг элементов

--- 01. Основы/Vue.1.0/src/App.vue
+++ 01. Основы/Vue.1.0/src/App.vue
@@ -2,24 +2,39 @@
 export default {
   data() {
     return {
-      visible: false,
+      visible1: false,
+      visible2: false,
+      visible3: false,
     }
   },
   methods: {
-    click: function() {
-      this.visible = true
+    toggle1: function() {
+      this.visible1 = !this.visible1;
     },
-
-    hide: function() {
-      this.visible = false
+    toggle2: function() {
+      this.visible2 = !this.visible2;
+    },
+    toggle3: function() {
+      this.visible3 = !this.visible3;
     }
+
+
   }
 }
 </script>
 
 <template>
-  <p v-if="visible">Hello World!</p>
-  <button v-if="!visible" @click="click">Показать</button>
-  <button v-if="visible" @click="hide">Скрыть</button>
+  <div>
+    <p v-if="visible1">Text1</p>
+    <button @click="toggle1">Click</button>
+  </div>
+  <div>
+    <p v-if="visible2">Text1</p>
+    <button @click="toggle2">Click</button>
+  </div>
+  <div>
+    <p v-if="visible3">Text1</p>
+    <button @click="toggle3">Click</button>
+  </div>
 </template>
 
