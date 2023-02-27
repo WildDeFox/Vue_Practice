cc26a9cafd6fbba2771c2fcaff95cce19b62f8d9
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-27 15:58:30 +0700

    Директива v-else

--- 01. Основы/Vue.1.0/src/App.vue
+++ 01. Основы/Vue.1.0/src/App.vue
@@ -2,39 +2,16 @@
 export default {
   data() {
     return {
-      visible1: false,
-      visible2: false,
-      visible3: false,
+      isAdmin: true
     }
   },
   methods: {
-    toggle1: function() {
-      this.visible1 = !this.visible1;
-    },
-    toggle2: function() {
-      this.visible2 = !this.visible2;
-    },
-    toggle3: function() {
-      this.visible3 = !this.visible3;
-    }
-
-
   }
 }
 </script>
 
 <template>
-  <div>
-    <p>{{ visible1 ? 'hide' : 'show' }}</p>
-    <button @click="toggle1">Click</button>
-  </div>
-  <div>
-    <p>{{ visible2 ? 'hide' : 'show' }}</p>
-    <button @click="toggle2">Click</button> 
-  </div>
-  <div>
-    <p>{{ visible3 ? 'hide' : 'show' }}</p>
-    <button @click="toggle3">Click</button>
-  </div>
+  <p v-if="isAdmin">Вы админ</p>
+  <p v-else>Вы не админ</p>
 </template>
 
