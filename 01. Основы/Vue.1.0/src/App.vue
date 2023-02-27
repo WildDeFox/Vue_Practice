43c01d93f8c2be52e515127853ca67a572308b95
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-27 17:52:06 +0700

    Двусторонняя привязка данных к инпутам

--- 01. Основы/Vue.1.0/src/App.vue
+++ 01. Основы/Vue.1.0/src/App.vue
@@ -2,48 +2,17 @@
 export default {
   data() {
     return {
-      products: [
-        {
-          id: 1,
-          name: "product1",
-        },
-        {
-          id: 2,
-          name: "product2",
-        },
-        {
-          id: 3,
-          name: "product3",
-        },
-      ],
-      cssClasses: {
-        red: false,
-        active: true
-      }
-    };
-  },
-  methods: {
-    click: function() {
-      this.cssClasses.red = !this.cssClasses.red;
-      this.cssClasses.active = !this.cssClasses.active;
+      message: 'Hello'
     }
   }
+  
 };
 </script>
 
 <template>
-  <ul>
-    <li :class="cssClasses" v-for="elem in products" :key="elem.id">{{ elem.name }}</li>
-  </ul>
-  <button @click="click">Click</button>
+  <input type="text" v-model="message">
+  <p>{{ message }}</p>
 </template>
 
 <style>
-.red {
-  color: red
-}
-
-.active {
-  color: yellow
-}
 </style>
