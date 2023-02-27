c4bec4d9a9df978a5fc1cef179b9e3136c29f169
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-27 16:29:02 +0700

    Перебор объектов

--- 01. Основы/Vue.1.0/src/App.vue
+++ 01. Основы/Vue.1.0/src/App.vue
@@ -2,20 +2,23 @@
 export default {
   data() {
     return {
-      arr: ['x', 'y', 'z']
-    }
+      obj: {
+        user1: "100$",
+        user2: "200$",
+        user3: "300$",
+      },
+    };
   },
   methods: {
-    hide: function() {
-      this.visible = !this.visible
-    }
-  }
-}
+    hide: function () {
+      this.visible = !this.visible;
+    },
+  },
+};
 </script>
 
 <template>
-  <ul v-for="(elem, key) in arr">
-    <li>{{ key }}</li>
+  <ul v-for="(elem, key, index) in obj">
+    <li>{{ key }} - {{ elem }} - {{ index + 1 }}</li>
   </ul>
 </template>
-
