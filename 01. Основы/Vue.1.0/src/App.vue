93843c2f0269aa7476e6d5430db3fd0336ec229d
WildMrDeFold <wildmrdefox@gmail.com>
2023-02-27 15:39:05 +0700

    Инвертирование условия

--- 01. Основы/Vue.1.0/src/App.vue
+++ 01. Основы/Vue.1.0/src/App.vue
@@ -3,14 +3,12 @@ export default {
   data() {
     return {
       visible1: false,
-      visible2: true,
     }
   },
 }
 </script>
 
 <template>
-  <div v-if="visible1">Hello world!</div>
-  <div v-if="visible2">How are you?</div>
+  <div v-if="!visible1">Hello world!</div>
 </template>
 
