08250fdadf1629fad4d80a0ce4ef51d3ee90995d
WildMrDeFold <wildmrdefox@gmail.com>
2023-03-02 18:28:46 +0700

    Создание компонентов в цикле

--- 01. Основы/Vue.1.0/src/App.vue
+++ 01. Основы/Vue.1.0/src/App.vue
@@ -1,24 +1,48 @@
 <script>
-  import User from './components/User.vue';
-  import Employee from './components/Employee.vue';
+import User from "./components/User.vue";
+import Employee from "./components/Employee.vue";
 
-  export default {
-    data() {
-      return {
-        name: 'Jhon',
-        surn: 'Smit',
-        age: '25',
-        salary: '80000'
-      }
-    },
-    components: {
-      User,
-      Employee
-    }
-  }
+export default {
+  data() {
+    return {
+      users: [
+        {
+          id: 1,
+          name: "Jhon",
+          surn: "Smit",
+          age: "18",
+          salary: "8000",
+        },
+        {
+          id: 2,
+          name: "Nikita",
+          surn: "Kapurin",
+          age: "25",
+          salary: "100000",
+        },
+        {
+          id: 3,
+          name: "Rick",
+          surn: "Sanchez",
+          age: "45",
+          salary: "80000",
+        },
+      ],
+    };
+  },
+  components: {
+    User,
+    Employee,
+  },
+};
 </script>
 
 <template>
-  <User name="Nikita" surn="Kapurin" :args="['item1', 'item2', 'item3']"/>
-  <Employee :name="name" :surn="surn" :age="age" :salary="salary"/>
+  <User name="Nikita" surn="Kapurin" :args="['item1', 'item2', 'item3']" />
+  <Employee v-for="user in users"
+  :name="user.name"
+  :surn="user.surn"
+  :age="user.age"
+  :salary="user.salary"
+  :key="user.id" />
 </template>
